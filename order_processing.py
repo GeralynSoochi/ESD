b3b4b916bee14f7c3bdbc17d2162edd13a7eb068
CHUA Jun Yong Shawn <shawn.chua.2018@sis.smu.edu.sg>
2020-03-29 19:34:37 +0800

    paypal

---
+++ order_processing.py
@@ -0,0 +1,45 @@
+import json
+import sys
+import os
+import urllib.request
+# Communication patterns:
+# Use a message-broker with 'direct' exchange to enable interaction
+import pika
+
+
+def receivePayment():
+    hostname = "localhost" # default broker hostname
+    port = 5672 # default port
+    # connect to the broker and set up a communication channel in the connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+    channel = connection.channel()
+
+    # set up the exchange if the exchange doesn't exist
+    exchangename="order_direct"
+    channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
+    # prepare a queue for receiving messages
+    channelqueue = channel.queue_declare(queue="payment", durable=True) # 'durable' makes the queue survive broker restarts
+    queue_name = channelqueue.method.queue
+    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='paymet.info') # bind the queue to the exchange via the key
+
+    # set up a consumer and start to wait for coming messages
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming() # an implicit loop waiting to receive messages; it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    # delete_cart(username)
+    print("Payment Success")
+
+# def processOrderError(order):
+#     print("Processing an order error:")
+#     print(order)
+
+# def delete_cart(username):
+#     urllib.request.urlopen('http://127.0.0.1:8005/cart/delete/'+ str(username))
+#     if (urllib.request.urlopen(req)):
+#         print("Deleted")
+
+if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')
+    print("This is " + os.path.basename(__file__) + ": processing payment...")
+    receivePayment()
--- payment.py
+++ payment.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template, jsonify, request
 import paypalrestsdk
 
+import json
+import sys
+import os
+import random
+import datetime
+import urllib.request
+
+# Communication patterns:
+# Use a message-broker with 'direct' exchange to enable interaction
+# Use a reply-to queue and correlation_id to get a corresponding reply
+import pika
+
+hostname = "localhost" # default hostname
+port = 5672 # default port
+# connect to the broker and set up a communication channel in the connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+    # Note: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
+    # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
+channel = connection.channel()
+# set up the exchange if the exchange doesn't exist
+exchangename="cart_direct"
+channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
 app = Flask(__name__)
 
 paypalrestsdk.configure({
@@ -15,7 +38,7 @@ def index():
 
 @app.route('/payment', methods=['POST'])
 def payment():
-
+    data = request.get_json()
     payment = paypalrestsdk.Payment({
         "intent": "sale",
         "payer": {
@@ -24,20 +47,20 @@ def payment():
             "return_url": "http://localhost:3000/payment/execute",
             "cancel_url": "http://localhost:3000/"},
         "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "testitem",
-                    "sku": "12345",
-                    "price": "500.00",
-                    "currency": "USD",
-                    "quantity": 1}]},
+            # "item_list": {
+            #     "items": [{
+            #         "name": "testitem",
+            #         "sku": "12345",
+            #         "price": "500.00",
+            #         "currency": "USD",
+            #         "quantity": 1}]},
             "amount": {
-                "total": "500.00",
+                "total": data['total'],
                 "currency": "USD"},
             "description": "This is the payment transaction description."}]})
 
     if payment.create():
-        print('Payment success!')
+        print('Login success!')
     else:
         print(payment.error)
 
@@ -46,16 +69,52 @@ def payment():
 @app.route('/execute', methods=['POST'])
 def execute():
     success = False
-
+    
     payment = paypalrestsdk.Payment.find(request.form['paymentID'])
 
     if payment.execute({'payer_id' : request.form['payerID']}):
         print('Execute success!')
         success = True
+        send_noti()
+
     else:
         print(payment.error)
 
     return jsonify({'success' : success})
+
+def send_noti():
+    # default username / password to the borker are both 'guest'
+    hostname = "localhost" # default broker hostname. Web management interface default at http://localhost:15672
+    port = 5672 # default messaging port.
+    # connect to the broker and set up a communication channel in the connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+        # Note: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
+        # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
+    channel = connection.channel()
+
+    # set up the exchange if the exchange doesn't exist
+    exchangename="order_direct"
+    channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
+    # prepare the message body content
+    message = "Payment success"
+
+    # send the message
+    # always inform Monitoring for logging no matter if successful or not
+    channel.basic_publish(exchange=exchangename, routing_key="payment.info", body=message)
+        # By default, the message is "transient" within the broker;
+        #  i.e., if the monitoring is offline or the broker cannot match the routing key for the message, the message is lost.
+        # If need durability of a message, need to declare the queue in the sender (see sample code below).
+    
+    # inform Error handler
+    channel.queue_declare(queue='payment', durable=True) # make sure the queue used by the error handler exist and durable
+    channel.queue_bind(exchange=exchangename, queue='payment', routing_key='payment.info') # make sure the queue is bound to the exchange
+    channel.basic_publish(exchange=exchangename, routing_key="payment.info", body=message,
+        properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
+    )
+    print("Message sent.")
+    connection.close()
+
 
 if __name__ == '__main__':
     app.run(port=5008, debug=True)
